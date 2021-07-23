7ea901e8c415e80800d641193cdc4bbeb99eb9c2
Your Name <leobarrientos02@gmail.com>
2021-07-23 17:58:34 -0400

    updates

--- app.js
+++ app.js
@@ -9,11 +9,21 @@ class DrumKit {
     // TRACKER
     this.index = 0;
   }
+
+  activePad() {
+    this.classList.toggle("active");
+  }
+
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
     //console.log(activeBars);
     this.index++;
+
+    // Loops over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out`;
+    });
   }
   start() {
     const interval = (60 / this.bpm) * 1000;
@@ -25,6 +35,10 @@ class DrumKit {
 // Constructor ENDS
 
 const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
 
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.start();
