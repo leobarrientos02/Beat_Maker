d6df8eac1a2e412ea9212351730e772fc3c4dfca
Your Name <leobarrientos02@gmail.com>
2021-07-24 12:52:16 -0400

    Pause added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ class DrumKit {
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.clapAudio = document.querySelector(".clap-sound");
     this.bpm = 150;
+    this.isPlaying = null;
     // TRACKER
     this.index = 0;
   }
@@ -48,9 +49,27 @@ class DrumKit {
   }
   start() {
     const interval = (60 / this.bpm) * 1000;
-    setInterval(() => {
-      this.repeat();
-    }, interval);
+    // Check if its playing
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+    } else {
+      // Clear the interval
+      clearInterval(this.isPlaying);
+      //console.log(isPlaying);
+      this.isPlaying = null;
+    }
+  }
+  // Update the button if no playing
+  updateBtn() {
+    if (!this.isPlaying) {
+      this.playBtn.innerText = "Stop";
+      this.playBtn.classList.add("active");
+    } else {
+      this.playBtn.innerText = "Play";
+      this.playBtn.classList.remove("active");
+    }
   }
 }
 // Constructor ENDS
@@ -66,5 +85,7 @@ drumKit.pads.forEach((pad) => {
 });
 
 drumKit.playBtn.addEventListener("click", function () {
+  // Update button when paused
+  drumKit.updateBtn();
   drumKit.start();
 });
